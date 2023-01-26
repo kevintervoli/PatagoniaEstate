e79e0c58fcdfa1feac9dfff9e8fe72804fe23d5c
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2023-01-26 22:45:05 +0100

    N

--- PHP/authenticate_user.php
+++ PHP/authenticate_user.php
@@ -11,7 +11,7 @@ if(isset($_POST['username'])){
 if(isset($_POST['password'])){
     $password = $_POST['password'];
 }
-$sql = "SELECT * FROM users WHERE username = '$username' AND pass = '$password'";
+$sql = "SELECT * FROM user WHERE username = '$username' AND pass = '$password'";
 
 $result = $conn->query($sql);
 
