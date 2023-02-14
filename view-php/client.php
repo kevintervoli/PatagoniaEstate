ddcfb0b752b0ed6a8690c5e5b8ecf4ecd96e91e6
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2023-02-14 16:30:28 +0100

    Client

--- view-php/client.php
+++ view-php/client.php
@@ -5,9 +5,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="./assets/css/style.css">
 </head>
 <body>
     <h1>Client Page</h1>
     Click here to <a href="../PHP/logout.php">Logout</a>
+    <div class="gallery">
+        
+    <?php include '../PHP/loadClient.php';?>
+    </div>
 </body>
 </html>
