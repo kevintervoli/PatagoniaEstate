666b5b69992a52bae9ff75a334cfed3000f8c04e
Kevin Tervoli <65734031+kevintervoli@users.noreply.github.com>
2023-02-14 21:07:14 +0100

    HTACCESS CHANGES

--- view-php/client.php
+++ view-php/client.php
@@ -5,13 +5,13 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
-    <link rel="stylesheet" href="./assets/css/style.css">
+    <link rel="stylesheet" href="../assets/css/style.css">
 </head>
 <body>
     <h1>Client Page</h1>
     Click here to <a href="../PHP/logout.php">Logout</a>
     <div class="gallery">
-    <!-- <?php include '../PHP/loadClient.php';?> -->
+    <?php include '../PHP/loadClient.php';?>
     </div>
 </body>
 </html>
